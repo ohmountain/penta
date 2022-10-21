40e629eb135e31bace56fde6c11dcf8d8950126f
renshan <1005110700@qq.com>
2022-10-21 18:07:45 +0800

    同步

--- penta.go
+++ penta.go
@@ -2,6 +2,8 @@ package penta
 
 import (
 	"context"
+	"errors"
+	"log"
 	"sync"
 
 	"github.com/ohmountain/penta/bb"
@@ -20,10 +22,32 @@ type penta struct {
 	status uint8
 }
 
-func (p *penta) Insert(proccesor bb.Processor[any, any]) {
+func (p *penta) Insert(proccesor bb.Processor[any, any]) error {
+
+	if p.status != pentaStatusStop {
+		return errors.New("Penta is running")
+	}
+
 	p.mu.Lock()
 	p.prs[proccesor.Id()] = proccesor
 	p.mu.Unlock()
+
+	return nil
+}
+
+func (p *penta) Join(proccesor bb.Processor[any, any]) error {
+
+	if p.status != pentaStatusRunning {
+		return errors.New("Penta not running")
+	}
+
+	p.mu.Lock()
+	p.prs[proccesor.Id()] = proccesor
+	p.mu.Unlock()
+
+	go proccesor.Run(p.ctx)
+
+	return nil
 }
 
 func (p *penta) Run() {
@@ -34,6 +58,10 @@ func (p *penta) Run() {
 	}
 	p.status = pentaStatusRunning
 	p.mu.Unlock()
+
+	if err := recover(); err != nil {
+		log.Printf("遇到了一些错误: %#v", err)
+	}
 
 	for {
 		select {
